a345b697987855c8323e419f5ec44b00252f31ad
Ratik Sharma <Ratik96@gmail.com>
2016-02-26 14:33:50 +0530

    Initial commit

---
+++ app/src/main/java/com/ratik/unsplashify/Constants.java
@@ -0,0 +1,28 @@
+package com.ratik.unsplashify;
+
+/**
+ * Created by Ratik on 26/02/16.
+ */
+public class Constants {
+
+    // Network constants
+    public static final String BASE_URL = "https://api.unsplash.com/photos/random/?client_id=";
+
+    // JSON API constants
+    public static final String CONST_WIDTH = "width";
+    public static final String CONST_HEIGHT = "height";
+
+    public static final String CONST_COLOR = "color";
+
+    public static final String CONST_URLS = "urls";
+    public static final String CONST_URLS_FULL = "full";
+    public static final String CONST_URLS_REGULAR = "regular";
+    public static final String CONST_URLS_SMALL = "small";
+    public static final String CONST_URLS_THUMB = "thumb";
+
+    public static final String CONST_USER = "user";
+    public static final String CONST_NAME = "name";
+
+    // Preference constants
+    public static final String PREF_REFRESH_INTERVAL = "refresh_interval";
+}
---
+++ app/src/main/java/com/ratik/unsplashify/ui/MainActivity.java
@@ -0,0 +1,15 @@
+package com.ratik.unsplashify.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.ratik.unsplashify.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
