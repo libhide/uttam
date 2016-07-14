a500ca8982830dc10a78cae211782c3d50c0609f
Ratik Sharma <Ratik96@gmail.com>
2016-07-14 22:30:02 +0530

    Add dev and prod product flavors

---
+++ app/src/dev/java/com/ratik/uttam/ui/ShowActivity.java
@@ -0,0 +1,164 @@
+package com.ratik.uttam.ui;
+
+import android.content.Intent;
+import android.content.res.Configuration;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.ratik.uttam.R;
+import com.ratik.uttam.asyncs.SetWallpaperTask;
+import com.ratik.uttam.utils.FileUtils;
+import com.ratik.uttam.utils.PhotoUtils;
+import com.ratik.uttam.utils.Utils;
+
+/**
+ * Created by Ratik on 29/02/16.
+ */
+public class ShowActivity extends AppCompatActivity {
+
+    private static final String TAG = ShowActivity.class.getSimpleName();
+
+    private int screenWidth;
+
+    private Bitmap wallpaper;
+
+    // Views
+    private ImageView image;
+    private TextView photographerTextView;
+    private ImageButton setWallpaperButton;
+    private LinearLayout creditsView;
+
+    private String photographer;
+    private String userProfileUrl;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_show);
+
+        screenWidth = Utils.getScreenWidth(this);
+
+        // Get photo data
+        wallpaper = FileUtils.getImageBitmap(this, "wallpaper", "png");
+        photographer = PhotoUtils.getPhotographerName(this);
+        userProfileUrl = PhotoUtils.getUserProf(this);
+
+        // Views init
+        image = (ImageView) findViewById(R.id.wallpaper);
+        photographerTextView = (TextView) findViewById(R.id.photographerTextView);
+        setWallpaperButton = (ImageButton) findViewById(R.id.wallpaperSetButton);
+        creditsView = (LinearLayout) findViewById(R.id.creditsContainer);
+
+        // Set photo data
+        image.setImageBitmap(wallpaper);
+        image.setOnTouchListener(imageScrollListener);
+        photographerTextView.setText(Utils.toTitleCase(photographer));
+
+        // Click listeners
+        setWallpaperButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new SetWallpaperTask(ShowActivity.this).execute(wallpaper);
+                // Finish the activity
+                finish();
+            }
+        });
+
+        creditsView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(userProfileUrl));
+                startActivity(browserIntent);
+            }
+        });
+    }
+
+    private View.OnTouchListener imageScrollListener = new View.OnTouchListener() {
+        float downX;
+        int totalX;
+        int scrollByX;
+
+        public boolean onTouch(View view, MotionEvent event) {
+            // set maximum scroll amount (based on center of image)
+            int maxX = ((wallpaper.getWidth() / 2)) - (screenWidth / 2 - 200);
+
+            // set scroll limits
+            final int maxLeft = (maxX * -1);
+            final int maxRight = maxX;
+
+            float currentX;
+
+            switch (event.getAction()) {
+                case MotionEvent.ACTION_DOWN:
+                    downX = event.getX();
+                    break;
+
+                case MotionEvent.ACTION_MOVE:
+                    currentX = event.getX();
+                    scrollByX = (int)(downX - currentX);
+
+                    // scrolling to left side of image (pic moving to the right)
+                    if (currentX > downX) {
+                        if (totalX == maxLeft) {
+                            scrollByX = 0;
+                        }
+                        if (totalX > maxLeft) {
+                            totalX = totalX + scrollByX;
+                        }
+                        if (totalX < maxLeft) {
+                            scrollByX = maxLeft - (totalX - scrollByX);
+                            totalX = maxLeft;
+                        }
+                    }
+
+                    // scrolling to right side of image (pic moving to the left)
+                    if (currentX < downX) {
+                        if (totalX == maxRight) {
+                            scrollByX = 0;
+                        }
+                        if (totalX < maxRight) {
+                            totalX = totalX + scrollByX;
+                        }
+                        if (totalX > maxRight) {
+                            scrollByX = maxRight - (totalX - scrollByX);
+                            totalX = maxRight;
+                        }
+                    }
+
+                    image.scrollBy(scrollByX, 0);
+                    downX = currentX;
+                    break;
+            }
+
+            return true;
+        }
+    };
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        int orientation = newConfig.orientation;
+        if (orientation == Configuration.ORIENTATION_PORTRAIT) {
+            image.scrollTo(0, 0);
+            image.setOnTouchListener(imageScrollListener);
+        } else {
+            image.scrollTo(0, 0);
+            image.setOnTouchListener(null);
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        finish();
+    }
+}
