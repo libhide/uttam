0872eb9742722027a379d47ab031388653502ada
Ratik Sharma <Ratik96@gmail.com>
2016-11-11 23:57:10 +0530

    Add changelog for user, update notif and more

---
+++ app/src/dev/java/com/ratik/uttam/ui/ChangelogDialog.java
@@ -0,0 +1,20 @@
+package com.ratik.uttam.ui;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.ratik.uttam.R;
+
+/**
+ * Created by Ratik on 11/11/16.
+ */
+
+public class ChangelogDialog extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_changelog);
+    }
+}
