dbd0ac6814fd7e402967a6a2dd515f68406f07d1
Ratik Sharma <Ratik96@gmail.com>
2018-01-22 21:11:53 +0530

    Refactor API call in Job which fetches wallpaper images

--- app/src/main/java/com/ratik/uttam/api/UnsplashService.java
+++ app/src/main/java/com/ratik/uttam/api/UnsplashService.java
@@ -2,7 +2,9 @@ package com.ratik.uttam.api;
 
 import com.ratik.uttam.model._Photo;
 
+import io.reactivex.Observable;
 import retrofit2.Call;
+import retrofit2.Response;
 import retrofit2.http.GET;
 import retrofit2.http.Query;
 
@@ -15,4 +17,8 @@ public interface UnsplashService {
     @GET("photos/random")
     Call<_Photo> getRandomPhoto(@Query("client_id") String clientId,
                                 @Query("category") String category);
+
+    @GET("photos/random")
+    Observable<Response<_Photo>> getRandomPhoto2(@Query("client_id") String clientId,
+                                                 @Query("category") String category);
 }
--- app/src/main/java/com/ratik/uttam/services/GetPhotoJob.java
+++ app/src/main/java/com/ratik/uttam/services/GetPhotoJob.java
@@ -4,6 +4,7 @@ import android.app.WallpaperManager;
 import android.app.job.JobParameters;
 import android.app.job.JobService;
 import android.content.Context;
+import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.util.Log;
 
@@ -22,7 +23,9 @@ import com.ratik.uttam.utils.Utils;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
 import java.net.URL;
+import java.security.Key;
 
 import javax.inject.Inject;
 
@@ -32,6 +35,7 @@ import io.reactivex.schedulers.Schedulers;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
+import retrofit2.http.Url;
 
 /**
  * Created by Ratik on 23/10/17.
@@ -63,81 +67,124 @@ public class GetPhotoJob extends JobService {
 
     @Override
     public boolean onStartJob(JobParameters jobParameters) {
-        getRandomPhoto(jobParameters);
+        fetchPhoto(jobParameters);
         return true;
     }
+
+    private void fetchPhoto(JobParameters parameters) {
+        Log.i(TAG, "Fetching photo...");
+//        service.getRandomPhoto(Keys.CLIENT_ID, FetchUtils.getRandomCategory())
+//                .enqueue(new Callback<_Photo>() {
+//                    @Override
+//                    public void onResponse(Call<_Photo> call, Response<_Photo> response) {
+//                        if (response.isSuccessful()) {
+//                            Log.i(TAG, "Photo fetched successfully!");
+//                            _Photo photo = response.body();
+//                            savePhoto(photo, parameters);
+//                        } else {
+//                            Log.i(TAG, "Photo fetch unsuccessful.");
+//                        }
+//                    }
+//
+//                    @Override
+//                    public void onFailure(Call<_Photo> call, Throwable t) {
+//                        Log.e(TAG, t.getMessage());
+//                    }
+//                });
+        service.getRandomPhoto2(Keys.CLIENT_ID, FetchUtils.getRandomCategory())
+                .map(photoResponse -> photoResponse.body())
+                .map(photo -> makePhotoObject(photo))
+                .map(photo -> dataStore.putPhoto(photo))
+                .observeOn(Schedulers.io())
+                .subscribeOn(AndroidSchedulers.mainThread())
+                .subscribe(
+                        (completable) -> {
+                            // Notify User
+                            notificationUtils.pushNewWallpaperNotif(p);
+
+                            // If user wants auto-magical setting, set the wallpaper
+                            if (PrefUtils.shouldSetWallpaperAutomatically(context)) {
+                                WallpaperManager.getInstance(context).setBitmap(image);
+                            }
+
+                            Log.i(TAG, "Photo saved successfully!");
+
+                            // job is done
+                            jobFinished(parameters, false);
+                        },
+                        (throwable -> {
+                            Log.e(TAG, throwable.getMessage());
+                        })
+                );
+    }
+
+    private Bitmap downloadWallpaperImage(_Photo photo) throws IOException {
+        try {
+            URL url = new URL(photo.getUrls().getFullUrl());
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setDoInput(true);
+            connection.connect();
+            InputStream input = connection.getInputStream();
+            return BitmapFactory.decodeStream(input);
+        } catch (MalformedURLException e) {
+            throw new IOException("Malformed URL for wallpaper image");
+        } catch (IOException e) {
+            throw new IOException("Error downloading wallpaper image");
+        }
+    }
+
+    private Photo makePhotoObject(_Photo photo) throws IOException {
+        Bitmap wallpaperImage = downloadWallpaperImage(photo);
+        if (wallpaperImage != null) {
+            // todo: implement builder pattern for Photo.class
+            Photo p = new Photo();
+            p.setPhotographerName(Utils.toTitleCase(photo.getPhotographer().getName()));
+            p.setPhotographerUserName(photo.getPhotographer().getUsername());
+            p.setPhotoDownloadUrl(photo.getLinks().getDownloadLink());
+            p.setPhotoHtmlUrl(photo.getLinks().getHtmlLink());
+            p.setPhotoFullUrl(photo.getUrls().getFullUrl());
+            p.setPhoto(wallpaperImage);
+            return p;
+        } else {
+            // todo: do something here
+            return null;
+        }
+    }
+
+
+//    private void savePhoto(_Photo photo, JobParameters parameters) {
+//        Observable.fromCallable(() -> {
+//            Log.i(TAG, "Url: " + url.toString());
+//
+//        })
+//                .map(bitmap -> BitmapUtils.scaleBitmap(context, bitmap))
+//                .subscribeOn(Schedulers.io())
+//                .observeOn(AndroidSchedulers.mainThread())
+//                .subscribe((image) -> {
+//                    if (image != null) {
+//
+//                        dataStore.putPhoto(p);
+//
+//                        // Notify User
+//                        notificationUtils.pushNewWallpaperNotif(p);
+//
+//                        // If user wants auto-magical setting, set the wallpaper
+//                        if (PrefUtils.shouldSetWallpaperAutomatically(context)) {
+//                            WallpaperManager.getInstance(context).setBitmap(image);
+//                        }
+//
+//                        Log.i(TAG, "Photo saved successfully!");
+//
+//                        // job is done
+//                        jobFinished(parameters, false);
+//                    } else {
+//                        Log.i(TAG, "Saving isn't working for some reason.");
+//                    }
+//                });
+//    }
 
     @Override
     public boolean onStopJob(JobParameters jobParameters) {
         return true;
-    }
-
-    private void getRandomPhoto(JobParameters parameters) {
-        // repository.clear();
-        Log.i(TAG, "Getting random photo...");
-        service.getRandomPhoto(Keys.CLIENT_ID, FetchUtils.getRandomCategory())
-                .enqueue(new Callback<_Photo>() {
-                    @Override
-                    public void onResponse(Call<_Photo> call, Response<_Photo> response) {
-                        if (response.isSuccessful()) {
-                            Log.i(TAG, "Photo fetched successfully!");
-                            _Photo photo = response.body();
-                            savePhoto(photo, parameters);
-                        }
-                    }
-
-                    @Override
-                    public void onFailure(Call<_Photo> call, Throwable t) {
-                        Log.e(TAG, "onFailure: " + t.getMessage());
-                    }
-                });
-    }
-
-    private void savePhoto(_Photo photo, JobParameters parameters) {
-        Observable.fromCallable(() -> {
-            URL url = new URL(photo.getUrls().getFullUrl());
-            Log.i(TAG, "Url: " + url.toString());
-            try {
-                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-                connection.setDoInput(true);
-                connection.connect();
-                InputStream input = connection.getInputStream();
-                return BitmapFactory.decodeStream(input);
-            } catch (IOException e) {
-                Log.e(TAG, e.getMessage());
-                return null;
-            }
-        })
-        .map(bitmap -> BitmapUtils.scaleBitmap(context, bitmap))
-        .subscribeOn(Schedulers.io())
-        .observeOn(AndroidSchedulers.mainThread())
-        .subscribe((image) -> {
-            if (image != null) {
-                Photo p = new Photo();
-                p.setPhotographerName(Utils.toTitleCase(photo.getPhotographer().getName()));
-                p.setPhotographerUserName(photo.getPhotographer().getUsername());
-                p.setPhotoDownloadUrl(photo.getLinks().getDownloadLink());
-                p.setPhotoHtmlUrl(photo.getLinks().getHtmlLink());
-                p.setPhotoFullUrl(photo.getUrls().getFullUrl());
-                p.setPhoto(image);
-
-                dataStore.putPhoto(p);
-
-                // Notify User
-                notificationUtils.pushNewWallpaperNotif(p);
-
-                // If user wants auto-magical setting, set the wallpaper
-                if (PrefUtils.shouldSetWallpaperAutomatically(context)) {
-                    WallpaperManager.getInstance(context).setBitmap(image);
-                }
-
-                Log.i(TAG, "Photo saved successfully!");
-
-                // job is done
-                jobFinished(parameters, false);
-            } else {
-                Log.i(TAG, "Saving isn't working for some reason.");
-            }
-        });
     }
 }
