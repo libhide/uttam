ab6a89c7349958d5504bc123fd3c92f6851f08fa
Ratik Sharma <Ratik96@gmail.com>
2018-01-29 00:10:04 +0530

    Update UnsplashService to make it return Observables

--- app/src/main/java/com/ratik/uttam/api/UnsplashService.java
+++ app/src/main/java/com/ratik/uttam/api/UnsplashService.java
@@ -3,7 +3,6 @@ package com.ratik.uttam.api;
 import com.ratik.uttam.model._Photo;
 
 import io.reactivex.Observable;
-import retrofit2.Call;
 import retrofit2.Response;
 import retrofit2.http.GET;
 import retrofit2.http.Query;
@@ -15,10 +14,6 @@ import retrofit2.http.Query;
 public interface UnsplashService {
 
     @GET("photos/random")
-    Call<_Photo> getRandomPhoto(@Query("client_id") String clientId,
-                                @Query("collections") String collections);
-
-    @GET("photos/random")
-    Observable<Response<_Photo>> getRandomPhoto2(@Query("client_id") String clientId,
+    Observable<Response<_Photo>> getRandomPhoto(@Query("client_id") String clientId,
                                                  @Query("collections") String collections);
 }
