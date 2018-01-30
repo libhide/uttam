c06a604430c30041f76d947434a84067fb24f522
Ratik Sharma <Ratik96@gmail.com>
2018-01-30 14:43:45 +0530

    Update image saving code

--- app/src/main/java/com/ratik/uttam/utils/BitmapUtils.java
+++ app/src/main/java/com/ratik/uttam/utils/BitmapUtils.java
@@ -4,6 +4,8 @@ import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 
+import com.ratik.uttam.model.Photo;
+
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 
@@ -11,6 +13,7 @@ import java.io.ByteArrayOutputStream;
  * Created by Ratik on 26/02/16.
  */
 public class BitmapUtils {
+
     static Bitmap cropToSquare(Bitmap bitmap) {
         int width = bitmap.getWidth();
         int height = bitmap.getHeight();
@@ -23,38 +26,40 @@ public class BitmapUtils {
         return Bitmap.createBitmap(bitmap, cropW, cropH, newWidth, newHeight);
     }
 
-    public static Bitmap scaleBitmap(Context context, Bitmap bm) {
+    public static Photo scaleBitmap(Context context, Photo photo) {
         int maxWidth = Utils.getScreenWidth(context);
         int maxHeight = Utils.getScreenHeight(context);
 
-        int width = bm.getWidth();
-        int height = bm.getHeight();
+        int width = photo.getPhoto().getWidth();
+        int height = photo.getPhoto().getHeight();
 
         if (width > height) {
             // landscape
             float ratio = (float) width / maxWidth;
             width = maxWidth;
-            height = (int)(height / ratio);
+            height = (int) (height / ratio);
         } else if (height > width) {
             // portrait
             float ratio = (float) height / maxHeight;
             height = maxHeight;
-            width = (int)(width / ratio);
+            width = (int) (width / ratio);
         } else {
             // square
             height = maxHeight;
             width = maxWidth;
         }
 
-        Bitmap bitmap = Bitmap.createScaledBitmap(bm, width, height, true);
+        Bitmap bitmap = Bitmap.createScaledBitmap(photo.getPhoto(), width, height, true);
 
         // Further compression
         ByteArrayOutputStream out = new ByteArrayOutputStream();
         if (PrefUtils.shouldCompressWallpaper(context)) {
-            bitmap.compress(Bitmap.CompressFormat.JPEG, 75, out);
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 50, out);
         } else {
-            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, out);
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 75, out);
         }
-        return BitmapFactory.decodeStream(new ByteArrayInputStream(out.toByteArray()));
+        Bitmap newBitmap = BitmapFactory.decodeStream(new ByteArrayInputStream(out.toByteArray()));
+        photo.setPhoto(newBitmap);
+        return photo;
     }
 }
