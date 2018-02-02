408234fd949e4a5c2d37fe80b70447e9d372b56c
Arun Sasidharan <arunsasidharan121@gmail.com>
2018-02-02 19:25:39 +0530

    refactored some code

--- app/src/main/java/com/ratik/uttam/services/GetPhotoService.java
+++ app/src/main/java/com/ratik/uttam/services/GetPhotoService.java
@@ -12,12 +12,16 @@ import com.ratik.uttam.Constants;
 import com.ratik.uttam.api.UnsplashService;
 import com.ratik.uttam.data.DataStore;
 import com.ratik.uttam.di.Injector;
+import com.ratik.uttam.model.Photo;
+import com.ratik.uttam.model.PhotoResponse;
 import com.ratik.uttam.utils.FetchUtils;
 import com.ratik.uttam.utils.NotificationUtils;
 import com.ratik.uttam.utils.PrefUtils;
+import com.ratik.uttam.utils.Utils;
 
 import javax.inject.Inject;
 
+import io.reactivex.Single;
 import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.schedulers.Schedulers;
 
@@ -63,24 +67,53 @@ public class GetPhotoService extends Service {
     private void fetchPhoto() {
         Log.i(TAG, "Getting random photo...");
         service.getRandomPhoto(BuildConfig.CLIENT_ID, Constants.Api.COLLECTIONS)
-                .map(photoResponse -> FetchUtils.makePhotoObject(this, photoResponse))
-                .map(photo -> dataStore.putPhoto(photo))
+                .flatMapSingle(response -> {
+                    Single<String> fullSingle = FetchUtils.downloadWallpaperFull(context, response);
+                    Single<String> regularSingle = FetchUtils.downloadWallpaperRegular(context, response);
+                    Single<String> thumbSingle = FetchUtils.downloadWallpaperThumb(context, response);
+
+                    return Single.zip(fullSingle, regularSingle, thumbSingle,
+                            (fullUri, regularUri, thumbUri) -> {
+                                Log.d(TAG, "Downloaded images");
+                                return getPhoto(response, fullUri, regularUri, thumbUri);
+                            });
+                })
+                .flatMapCompletable(photo -> dataStore.putPhoto(photo))
                 .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
-                .subscribe((completable) -> {
-                    // Notify User
-                    notificationUtils.pushNewWallpaperNotification();
+                .subscribe(this::onFetchSuccess, this::onFetchFailure);
+    }
 
-                    // If user wants auto-magical setting, set the wallpaper
-                    if (PrefUtils.shouldSetWallpaperAutomatically(context)) {
-                        // todo: figure out automagical setting
-                        // WallpaperManager.getInstance(context).setBitmap(image);
-                    }
+    private void onFetchSuccess() {
+        // Notify User
+        notificationUtils.pushNewWallpaperNotification();
 
-                    Log.i(TAG, "Photo saved successfully!");
+        // If user wants auto-magical setting, set the wallpaper
+        if (PrefUtils.shouldSetWallpaperAutomatically(context)) {
+            // todo: figure out automagical setting
+            // WallpaperManager.getInstance(context).setBitmap(image);
+        }
 
-                    // stop service
-                    stopSelf();
-                }, throwable -> Log.e(TAG, throwable.getMessage()));
+        Log.i(TAG, "Photo saved successfully!");
+
+        // stop service
+        stopSelf();
+    }
+
+    private void onFetchFailure(Throwable throwable) {
+        Log.e(TAG, throwable.getMessage());
+    }
+
+    private Photo getPhoto(PhotoResponse response, String fullUri, String regularUri, String thumbUri) {
+        return new Photo.Builder()
+                .setPhotoUri(fullUri)
+                .setRegularPhotoUri(regularUri)
+                .setThumbPhotoUri(thumbUri)
+                .setPhotoFullUrl(response.getUrls().getFullUrl())
+                .setPhotoHtmlUrl(response.getLinks().getHtmlLink())
+                .setPhotoDownloadUrl(response.getLinks().getDownloadLink())
+                .setPhotographerUserName(response.getPhotographer().getUsername())
+                .setPhotographerName(Utils.toTitleCase(response.getPhotographer().getName()))
+                .build();
     }
 }
