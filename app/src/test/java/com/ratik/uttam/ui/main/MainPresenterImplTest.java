18c1c09966b4f9411cc7d54e92615e747e860759
Ratik Sharma <Ratik96@gmail.com>
2018-09-05 21:35:41 +0530

    Update MainPresenter tests

--- app/src/test/java/com/ratik/uttam/ui/main/MainPresenterImplTest.java
+++ app/src/test/java/com/ratik/uttam/ui/main/MainPresenterImplTest.java
@@ -2,6 +2,7 @@ package com.ratik.uttam.ui.main;
 
 import com.ratik.uttam.data.PhotoStore;
 import com.ratik.uttam.model.Photo;
+import com.ratik.uttam.network.FetchService;
 
 import org.junit.Before;
 import org.junit.Rule;
@@ -9,11 +10,15 @@ import org.junit.Test;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
-import java.io.IOException;
-
+import io.reactivex.Completable;
 import io.reactivex.Single;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
@@ -31,43 +36,82 @@ public class MainPresenterImplTest {
     @Mock
     private PhotoStore photoStore;
 
-    private MainPresenterImpl presenter;
+    @Mock
+    private FetchService service;
 
+    private MainPresenterImpl presenter;
     private Photo testPhoto;
 
     @Before
-    public void setUp() throws Exception {
+    public void setUp() {
         MockitoAnnotations.initMocks(this);
-
-        presenter = new MainPresenterImpl(photoStore);
+        presenter = new MainPresenterImpl(photoStore, service);
         presenter.setView(view);
-
         testPhoto = new Photo.Builder().build();
     }
 
     @Test
     public void shouldLoadPhotoIntoView() {
-        // given
-        when(photoStore.getPhoto()).thenReturn(Single.just(testPhoto));
+        when(photoStore.getPhoto())
+                .thenReturn(Single.just(testPhoto));
 
-        // when
         presenter.getPhoto();
 
-        // then
         verify(view).showPhoto(testPhoto);
         verify(view, never()).onGetPhotoFailed();
     }
 
     @Test
-    public void shouldShowErrorView() {
-        // given
-        when(photoStore.getPhoto()).thenReturn(Single.error(new IOException()));
+    public void shouldShowPhotoLoadErrorInView() {
+        when(photoStore.getPhoto())
+                .thenReturn(Single.error(new Exception()));
 
-        // when
         presenter.getPhoto();
 
-        // then
         verify(view).onGetPhotoFailed();
         verify(view, never()).showPhoto(testPhoto);
+    }
+
+    @Test
+    public void viewShouldAttach() {
+        presenter.setView(view);
+        assertNotNull(presenter.getView());
+    }
+
+    @Test
+    public void viewShouldDetach() {
+        presenter.detachView();
+        assertNull(presenter.getView());
+    }
+
+    @Test
+    public void disposablesShouldDispose() {
+        presenter.detachView();
+        assertEquals(true, presenter.getCompositeDisposable().isDisposed());
+    }
+
+    @Test
+    public void shouldRefetchPhotoSuccessfully() {
+        when(service.getFetchPhotoCompletable())
+                .thenReturn(Completable.complete());
+
+        presenter.refetchPhoto();
+
+        verify(view, times(1)).showRefetchProgress();
+        verify(view, times(1)).hideRefetchProgress();
+        verify(view, never()).showRefetchError(anyString());
+    }
+
+    @Test
+    public void shouldRefetchPhotoUnsuccessfully() {
+        when(service.getFetchPhotoCompletable())
+                .thenReturn(Completable.error(new Throwable()));
+
+        presenter.refetchPhoto();
+
+        verify(view).showRefetchError(anyString());
+        verify(view, times(1)).showRefetchProgress();
+        verify(view, never()).hideRefetchProgress();
+
     }
 }
