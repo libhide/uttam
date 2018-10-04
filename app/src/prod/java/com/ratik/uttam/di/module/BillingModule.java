8384e9b06c09e62522a6ce5697e076844ef826b4
Ratik Sharma <Ratik96@gmail.com>
2018-10-04 11:40:03 +0530

    Move billing public key into Constants.java

--- app/src/prod/java/com/ratik/uttam/di/module/BillingModule.java
+++ app/src/prod/java/com/ratik/uttam/di/module/BillingModule.java
@@ -2,6 +2,8 @@ package com.ratik.uttam.di.module;
 
 import android.content.Context;
 
+import com.ratik.uttam.Constants;
+
 import org.solovyev.android.checkout.Billing;
 
 import javax.annotation.Nonnull;
@@ -18,8 +20,7 @@ public class BillingModule {
             @Nonnull
             @Override
             public String getPublicKey() {
-                // TODO: learn to encrypt the key
-                return "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAoueA0HGGz3Y0XE6WmVT7jt4e7cDhMWoH99mpAiu4uJjHOMR3dlpJOeK4GwayFKiemVHdQSu7hxNzVBYZdX09VWCrPGeFJfme6lMXP9QmWJKyyivPuzUW4w/0eEl908hJEJIgyJkR89LSfNVEmwrVo3mM4ZI6hXks/libNpaKiny1y2LLWlEM3Ue9nVqhj68G/iWIrF1Zk6h7K3lvF4mj7MEnTsFbVZzuGtFFOuibQXd9d1Jib3HCrApRhYXnhsi3yszCv4BErsBG2F55mJmpp7fGD37RIlTIx/PR+xOISjzliwM010REDmVpNFVRzep27Q2La3GAv4eCFf7tqeCSwQIDAQAB";
+                return Constants.Billing.BILLING_PUBLIC_KEY;
             }
         };
     }
