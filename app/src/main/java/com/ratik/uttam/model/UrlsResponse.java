6f76aa8530ac1585a5d4b940aa45fc78d23ddf6e
Ratik Sharma <ratik96@gmail.com>
2022-10-25 22:30:42 +0530

    fix(*): update unsplash response schema

--- app/src/main/java/com/ratik/uttam/model/UrlsResponse.java
+++ app/src/main/java/com/ratik/uttam/model/UrlsResponse.java
@@ -8,7 +8,7 @@ import com.google.gson.annotations.SerializedName;
 
 public class UrlsResponse {
 
-    @SerializedName("custom")
+    @SerializedName("full")
     private String fullUrl;
 
     @SerializedName("regular")
