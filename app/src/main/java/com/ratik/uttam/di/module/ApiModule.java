52ec3571b6ba82e71d1137251b973f9a69c216b0
Ratik Sharma <ratik96@gmail.com>
2022-10-25 22:30:20 +0530

    chore(*): bump deps

--- app/src/main/java/com/ratik/uttam/di/module/ApiModule.java
+++ app/src/main/java/com/ratik/uttam/di/module/ApiModule.java
@@ -1,10 +1,15 @@
 package com.ratik.uttam.di.module;
 
+import com.ratik.uttam.BuildConfig;
 import com.ratik.uttam.Constants;
 import com.ratik.uttam.api.UnsplashService;
 
+import javax.inject.Singleton;
+
 import dagger.Module;
 import dagger.Provides;
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
 import retrofit2.converter.gson.GsonConverterFactory;
@@ -17,8 +22,27 @@ import retrofit2.converter.gson.GsonConverterFactory;
 public class ApiModule {
 
     @Provides
-    public Retrofit provideRetrofit() {
+    @Singleton
+    HttpLoggingInterceptor provideLoggingInterceptor() {
+        return new HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY);
+    }
+
+    @Provides
+    @Singleton
+    OkHttpClient provideOkHttpClient(HttpLoggingInterceptor loggingInterceptor) {
+        OkHttpClient.Builder okhttpClientBuilder = new OkHttpClient.Builder();
+
+        if (BuildConfig.DEBUG) {
+            okhttpClientBuilder.addInterceptor(loggingInterceptor);
+        }
+
+        return okhttpClientBuilder.build();
+    }
+
+    @Provides
+    public Retrofit provideRetrofit(OkHttpClient client) {
         return new Retrofit.Builder()
+                .client(client)
                 .baseUrl(Constants.Api.BASE_URL)
                 .addConverterFactory(GsonConverterFactory.create())
                 .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
