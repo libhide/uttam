ed1ea95c2324cdf518bbc27f862a49c0df7f35e7
Ratik Sharma <ratik96@gmail.com>
2024-04-14 03:13:09 +0530

    chore(*): connect compose screen with VM

--- app/src/main/kotlin/com/ratik/uttam/ui/home/HomeScreen.kt
+++ app/src/main/kotlin/com/ratik/uttam/ui/home/HomeScreen.kt
@@ -16,20 +16,33 @@ import androidx.compose.foundation.layout.safeDrawingPadding
 import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.core.content.ContextCompat
 import androidx.core.content.FileProvider
+import androidx.hilt.navigation.compose.hiltViewModel
+import com.fueled.android.core.ui.extensions.collectAsEffect
+import com.fueled.android.core.ui.extensions.rememberFlowOnLifecycle
 import com.ratik.uttam.R
+import com.ratik.uttam.core.Ignored
+import com.ratik.uttam.core.MessageState
+import com.ratik.uttam.core.MessageState.Snack
+import com.ratik.uttam.core.contract.ViewEvent
+import com.ratik.uttam.core.contract.ViewEvent.DisplayMessage
 import com.ratik.uttam.ui.components.ScrollableImage
+import com.ratik.uttam.ui.home.HomeAction.RefreshWallpaper
 import com.ratik.uttam.ui.theme.Dimens.SpacingNormal
 import com.ratik.uttam.ui.theme.Dimens.TextSizeXXLarge
 import java.io.File
 
 @Composable
-internal fun HomeScreen() {
+internal fun HomeScreen(
+    viewModel: HomeViewModel = hiltViewModel(),
+) {
     val context = LocalContext.current
     val wallpaper = painterResource(id = R.drawable.uttam_hero)
     val wallpaperManager = WallpaperManager.getInstance(context)
@@ -39,6 +52,25 @@ internal fun HomeScreen() {
                 Toast.makeText(context, R.string.wallpaper_set_text, Toast.LENGTH_SHORT).show()
             }
         }
+
+    val state by rememberFlowOnLifecycle(flow = viewModel.state)
+        .collectAsState(HomeState.initialState)
+
+    viewModel.events.collectAsEffect { event ->
+        when (event) {
+            is DisplayMessage -> {
+                when (event.message) {
+                    is Snack -> {
+                        Toast.makeText(context, event.message.message, Toast.LENGTH_SHORT).show()
+                    }
+
+                    else -> Ignored
+                }
+            }
+
+            else -> Ignored
+        }
+    }
 
     ScrollableImage(
         painter = wallpaper,
@@ -52,41 +84,42 @@ internal fun HomeScreen() {
     ) {
         Button(
             onClick = {
+                viewModel.onViewAction(RefreshWallpaper)
                 // Setting with intent
-                val drawable = ContextCompat.getDrawable(context, R.drawable.uttam_hero)
-
-                val bitmap = (drawable?.mutate() as BitmapDrawable).bitmap.copy(
-                    Bitmap.Config.ARGB_8888,
-                    true
-                )
-
-                val canvas = Canvas(bitmap)
-
-                drawable.setBounds(0, 0, drawable.intrinsicWidth, drawable.intrinsicHeight)
-
-                drawable.draw(canvas)
-
-                val tempFile = File.createTempFile(
-                    "wallpaper",
-                    ".jpg",
-                    context.cacheDir
-                )
-
-                val outputStream = tempFile.outputStream()
-
-                bitmap.compress(Bitmap.CompressFormat.JPEG, 100, outputStream)
-
-                outputStream.close()
-
-                val uri = FileProvider.getUriForFile(
-                    context,
-                    "${context.packageName}.provider",
-                    tempFile
-                )
-
-                val wallpaperSetIntent = wallpaperManager.getCropAndSetWallpaperIntent(uri)
-
-                launcher.launch(wallpaperSetIntent)
+//                val drawable = ContextCompat.getDrawable(context, R.drawable.uttam_hero)
+//
+//                val bitmap = (drawable?.mutate() as BitmapDrawable).bitmap.copy(
+//                    Bitmap.Config.ARGB_8888,
+//                    true
+//                )
+//
+//                val canvas = Canvas(bitmap)
+//
+//                drawable.setBounds(0, 0, drawable.intrinsicWidth, drawable.intrinsicHeight)
+//
+//                drawable.draw(canvas)
+//
+//                val tempFile = File.createTempFile(
+//                    "wallpaper",
+//                    ".jpg",
+//                    context.cacheDir
+//                )
+//
+//                val outputStream = tempFile.outputStream()
+//
+//                bitmap.compress(Bitmap.CompressFormat.JPEG, 100, outputStream)
+//
+//                outputStream.close()
+//
+//                val uri = FileProvider.getUriForFile(
+//                    context,
+//                    "${context.packageName}.provider",
+//                    tempFile
+//                )
+//
+//                val wallpaperSetIntent = wallpaperManager.getCropAndSetWallpaperIntent(uri)
+//
+//                launcher.launch(wallpaperSetIntent)
 
                 // Setting directly
 //                        val drawable = ContextCompat.getDrawable(context, R.drawable.uttam_hero)
--- app/src/main/kotlin/com/ratik/uttam/ui/home/HomeViewModel.kt
+++ app/src/main/kotlin/com/ratik/uttam/ui/home/HomeViewModel.kt
@@ -2,8 +2,15 @@ package com.ratik.uttam.ui.home
 
 import com.ratik.uttam.core.BaseViewModel
 import com.ratik.uttam.core.DispatcherProvider
+import com.ratik.uttam.core.MessageState
+import com.ratik.uttam.core.MessageState.Snack
+import com.ratik.uttam.core.contract.ViewEvent
+import com.ratik.uttam.core.contract.ViewEvent.DisplayMessage
+import com.ratik.uttam.ui.home.HomeAction.RefreshWallpaper
+import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
+@HiltViewModel
 internal class HomeViewModel @Inject constructor(
     dispatcherProvider: DispatcherProvider,
 ) : BaseViewModel<HomeState, HomeAction>(
@@ -12,10 +19,12 @@ internal class HomeViewModel @Inject constructor(
 ) {
 
     override fun onViewAction(viewAction: HomeAction) {
-        TODO("Not yet implemented")
+        when (viewAction) {
+            RefreshWallpaper -> {
+                dispatchViewEvent(DisplayMessage(Snack("Refreshing...")))
+            }
+        }
     }
 
-    override fun handleError(throwable: Throwable) {
-        TODO("Not yet implemented")
-    }
+    override fun handleError(throwable: Throwable) = Unit
 }
