93fbd71ed8e4167f96ca2b1347823b5e3aa99008
Ratik Sharma <ratik96@gmail.com>
2024-06-06 19:25:10 +0530

    feat(nav): set up app navigation

--- app/src/main/kotlin/com/ratik/uttam/ui/MainApp.kt
+++ app/src/main/kotlin/com/ratik/uttam/ui/MainApp.kt
@@ -1,20 +1,38 @@
 package com.ratik.uttam.ui
 
+import android.annotation.SuppressLint
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.navigationBarsPadding
+import androidx.compose.foundation.layout.systemBarsPadding
+import androidx.compose.material.Scaffold
 import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import com.ratik.uttam.ui.landing.LandingScreen
+import androidx.navigation.compose.rememberNavController
+import com.ratik.uttam.navigation.MainNavHost
 import com.ratik.uttam.ui.theme.UttamTheme
 
+@SuppressLint("UnusedMaterialScaffoldPaddingParameter")
 @Composable
 internal fun MainApp() {
+    val navController = rememberNavController()
+    var toolbarText by rememberSaveable { mutableStateOf("") }
+
     UttamTheme {
-        Surface(
-            modifier = Modifier.fillMaxSize(),
-        ) {
-            // HomeScreen()
-            LandingScreen()
+        Scaffold(
+            topBar = {
+                // TODO: show top bar only on the Settings screen
+            },
+        ) { _ ->
+            MainNavHost(
+                navController = navController,
+                setToolbarTitle = { title -> toolbarText = title },
+            )
         }
     }
 }
