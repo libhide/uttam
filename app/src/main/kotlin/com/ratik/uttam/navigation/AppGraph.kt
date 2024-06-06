145d8d019b7df1e5f7c47db83a35e5bf809e3485
Ratik Sharma <ratik96@gmail.com>
2024-06-06 19:39:51 +0530

    feat(nav): set up app navigation (#48)

---
+++ app/src/main/kotlin/com/ratik/uttam/navigation/AppGraph.kt
@@ -0,0 +1,40 @@
+package com.ratik.uttam.navigation
+
+import androidx.navigation.NavGraphBuilder
+import androidx.navigation.compose.composable
+import com.ratik.uttam.navigation.model.Destination
+import com.ratik.uttam.navigation.model.Graph
+import com.ratik.uttam.ui.home.HomeScreen
+import com.ratik.uttam.ui.landing.LandingScreen
+
+object AppGraph : Graph("uttam")
+
+sealed class AppDestination {
+
+    object Landing : Destination("landing")
+
+    object Home : Destination("home")
+}
+
+fun NavGraphBuilder.addLandingScreen(
+    graph: Graph,
+    onGetStarted: () -> Unit,
+) {
+    composable(
+        route = AppDestination.Landing.createRoute(graph),
+    ) {
+        LandingScreen(
+            onGetStarted = onGetStarted,
+        )
+    }
+}
+
+fun NavGraphBuilder.addHomeScreen(
+    graph: Graph,
+) {
+    composable(
+        route = AppDestination.Home.createRoute(graph),
+    ) {
+        HomeScreen()
+    }
+}
---
+++ app/src/main/kotlin/com/ratik/uttam/navigation/MainNavHost.kt
@@ -0,0 +1,68 @@
+package com.ratik.uttam.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavGraphBuilder
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.navigation
+import com.ratik.uttam.navigation.model.Graph
+import com.ratik.uttam.navigation.model.GraphSaver
+
+/**
+ * This function sets up the main navigation graph.
+ * As we have a bottom navigation bar, this sets different navigation graphs for all the bottom items.
+ *
+ * Based on the current business logic (having a bottom navigation bar) we set a sub-graph
+ * for each bottom navigation item, so in order to preserve a back stack for each bottom navigation
+ * item.
+ *
+ * Depending on different business requirements (such as different navigation type of not having a
+ * bottom navigation at all) it can happen that there is no need for this level of complexity and
+ * everything can be added to a single navigation graph.
+ */
+@Composable
+internal fun MainNavHost(
+    modifier: Modifier = Modifier,
+    navController: NavHostController,
+    setToolbarTitle: (String) -> Unit,
+) {
+    val rootGraph by rememberSaveable(stateSaver = GraphSaver) { mutableStateOf(AppGraph) }
+
+    NavHost(
+        navController = navController,
+        modifier = modifier,
+        startDestination = rootGraph.route,
+    ) {
+        addAppGraph(
+            navController = navController,
+            setToolbarTitle = setToolbarTitle
+        )
+    }
+}
+
+private fun NavGraphBuilder.addAppGraph(
+    graph: Graph = AppGraph,
+    navController: NavHostController,
+    setToolbarTitle: (String) -> Unit,
+) {
+    navigation(
+        route = graph.route,
+        startDestination = AppDestination.Landing.createRoute(graph),
+    ) {
+        addLandingScreen(
+            graph = graph,
+            onGetStarted = {
+                navController.navigate(AppDestination.Home.createRoute(graph)) {
+                    popUpTo(AppDestination.Landing.createRoute(graph)) {
+                        inclusive = true
+                    }
+                }
+            },
+        )
+        addHomeScreen(graph)
+    }
+}
