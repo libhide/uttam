ec3cb33ae308b5533aad3491b2329a0bbfe3bfba
Ratik Sharma <ratik96@gmail.com>
2024-06-08 01:28:13 +0530

    fix(home): add missing modifier (#49)

--- app/src/main/kotlin/com/ratik/uttam/ui/home/HomeScreen.kt
+++ app/src/main/kotlin/com/ratik/uttam/ui/home/HomeScreen.kt
@@ -11,6 +11,8 @@ import androidx.compose.foundation.layout.Arrangement.SpaceBetween
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.systemBarsPadding
@@ -102,6 +104,7 @@ internal fun HomeScreen(
     Column(
         verticalArrangement = SpaceBetween,
         modifier = Modifier
+            .fillMaxSize()
             .systemBarsPadding()
             .padding(
                 horizontal = SpacingMedium,
