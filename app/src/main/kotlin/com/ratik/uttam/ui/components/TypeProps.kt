424a6170ae13577395e347dbbcac6b238dc510d1
Ratik Sharma <ratik96@gmail.com>
2024-06-07 21:04:37 +0530

    feat(text-component): create global text component

---
+++ app/src/main/kotlin/com/ratik/uttam/ui/components/TypeProps.kt
@@ -0,0 +1,28 @@
+package com.ratik.uttam.ui.components
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.font.FontWeight.Companion.Bold
+import androidx.compose.ui.text.font.FontWeight.Companion.Normal
+import androidx.compose.ui.unit.TextUnit
+import com.ratik.uttam.ui.components.UttamText.Body
+import com.ratik.uttam.ui.components.UttamText.BodyBold
+import com.ratik.uttam.ui.theme.Dimens.TextSizeDefault
+
+internal data class TypeProps(
+    val fontWeight: FontWeight,
+    val textSize: TextUnit,
+)
+
+@Composable
+internal fun getTypeProps(text: UttamText): TypeProps = when (text) {
+    Body -> TypeProps(
+        fontWeight = Normal,
+        textSize = TextSizeDefault,
+    )
+
+    BodyBold -> TypeProps(
+        fontWeight = Bold,
+        textSize = TextSizeDefault,
+    )
+}
---
+++ app/src/main/kotlin/com/ratik/uttam/ui/components/UttamText.kt
@@ -0,0 +1,74 @@
+package com.ratik.uttam.ui.components
+
+import androidx.compose.material.LocalTextStyle
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.AnnotatedString
+import androidx.compose.ui.text.TextLayoutResult
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.TextUnit
+import com.ratik.uttam.ui.theme.TextColor
+
+/**
+ * Core Text UI components across the six flags application. Renders the correct text style
+ * based on the enum type declared.
+ *
+ * Can be used like the following:
+ * ```
+ * UttamText.H4(text = "Hello, World!")
+ * UttamText.Body(text = "Hello, World!")
+ * ```
+ */
+enum class UttamText {
+    Body, BodyBold;
+
+    @Composable
+    operator fun invoke(
+        modifier: Modifier = Modifier,
+        text: String,
+        annotatedText: AnnotatedString? = null,
+        textAlign: TextAlign = TextAlign.Start,
+        textColor: Color = TextColor,
+        textSize: TextUnit? = null,
+        maxLines: Int = Int.MAX_VALUE,
+        letterSpacing: TextUnit = TextUnit.Unspecified,
+        onTextLayout: (TextLayoutResult) -> Unit = {},
+        textDecoration: TextDecoration = TextDecoration.None,
+    ) {
+        val typeProps = getTypeProps(this)
+        val textStyle = LocalTextStyle.current
+        if (annotatedText != null) {
+            Text(
+                modifier = modifier,
+                text = annotatedText,
+                textAlign = textAlign,
+                color = textColor,
+                maxLines = maxLines,
+                fontSize = textSize ?: typeProps.textSize,
+                fontWeight = typeProps.fontWeight,
+                letterSpacing = letterSpacing,
+                overflow = TextOverflow.Ellipsis,
+                onTextLayout = onTextLayout,
+                style = textStyle.copy(textDecoration = textDecoration),
+            )
+        } else {
+            Text(
+                modifier = modifier,
+                text = text,
+                textAlign = textAlign,
+                color = textColor,
+                maxLines = maxLines,
+                fontSize = textSize ?: typeProps.textSize,
+                fontWeight = typeProps.fontWeight,
+                letterSpacing = letterSpacing,
+                overflow = TextOverflow.Ellipsis,
+                onTextLayout = onTextLayout,
+                style = textStyle.copy(textDecoration = textDecoration),
+            )
+        }
+    }
+}
