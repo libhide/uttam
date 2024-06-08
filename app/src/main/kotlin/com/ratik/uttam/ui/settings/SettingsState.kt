dc4cf5f486705e8e11630920500b89d9de2caf0a
Ratik Sharma <ratik96@gmail.com>
2024-06-08 18:48:40 +0530

    feat(settings): implement vm and state for settings

--- app/src/main/kotlin/com/ratik/uttam/ui/settings/SettingsState.kt
+++ app/src/main/kotlin/com/ratik/uttam/ui/settings/SettingsState.kt
@@ -1,2 +1,14 @@
 package com.ratik.uttam.ui.settings
 
+import com.ratik.uttam.core.MessageState
+import com.ratik.uttam.core.contract.BaseState
+
+internal data class SettingsState(
+    override val isLoading: Boolean = false,
+    override val errorState: MessageState? = null,
+    val setWallpaperAutomatically: Boolean = true,
+) : BaseState {
+    companion object {
+        val initialState = SettingsState()
+    }
+}
