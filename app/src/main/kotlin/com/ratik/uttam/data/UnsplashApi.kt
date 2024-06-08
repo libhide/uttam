adc32fb727e80899747d99f3ca99a6006e09cfb9
Ratik Sharma <ratik96@gmail.com>
2024-06-09 01:29:00 +0530

    chore(*): fix broken compilation caused by git issue

--- app/src/main/kotlin/com/ratik/uttam/data/UnsplashApi.kt
+++ app/src/main/kotlin/com/ratik/uttam/data/UnsplashApi.kt
@@ -1,7 +1,6 @@
 package com.ratik.uttam.data
 
 import com.ratik.uttam.data.model.PhotoApiModel
-import io.reactivex.Observable
 import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Path
@@ -11,15 +10,6 @@ import retrofit2.http.Query
  * Created by Ratik on 17/10/17.
  */
 interface UnsplashApi {
-    @Deprecated("Use getRandomPhotoSus instead")
-    @GET("photos/random")
-    fun getRandomPhoto(
-        @Query("client_id") clientId: String?,
-        @Query("collections") collections: String?,
-        @Query("w") w: Int,
-        @Query("h") h: Int
-    ): Observable<PhotoApiModel?>?
-
     @GET("photos/random")
     suspend fun getRandomPhotoSus(
         @Query("client_id") clientId: String?,
