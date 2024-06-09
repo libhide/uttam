236e9af9ebc621bbf73db52e42ef90a38f5c86ac
Ratik Sharma <ratik96@gmail.com>
2024-06-10 02:00:06 +0530

    feat(download-count): implement unsplash download count increment (#81)

--- app/src/main/kotlin/com/ratik/uttam/data/UnsplashApi.kt
+++ app/src/main/kotlin/com/ratik/uttam/data/UnsplashApi.kt
@@ -5,6 +5,7 @@ import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Path
 import retrofit2.http.Query
+import retrofit2.http.Url
 
 /** Created by Ratik on 17/10/17. */
 interface UnsplashApi {
@@ -23,4 +24,10 @@ interface UnsplashApi {
     @Query("w") w: Int? = null,
     @Query("h") h: Int? = null,
   ): Response<PhotoApiModel>
+
+  @GET
+  suspend fun incrementDownloadCount(
+    @Url url: String,
+    @Query("client_id") clientId: String?,
+  ): Response<Unit>
 }
