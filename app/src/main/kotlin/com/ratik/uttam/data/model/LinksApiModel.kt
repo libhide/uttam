2d8472201f83aff852331a7c2655dcbd0fc7cdb6
Ratik Sharma <ratik96@gmail.com>
2024-06-10 01:51:58 +0530

    feat(download-count): implement unsplash download count increment

--- app/src/main/kotlin/com/ratik/uttam/data/model/LinksApiModel.kt
+++ app/src/main/kotlin/com/ratik/uttam/data/model/LinksApiModel.kt
@@ -5,8 +5,5 @@ import com.google.gson.annotations.SerializedName
 /** Created by Ratik on 17/10/17. */
 data class LinksApiModel(
   @SerializedName("html") val htmlLink: String,
-  @Deprecated("Redundant field, will be removed")
-  @SerializedName("download")
-  val downloadLink: String? = null,
-  @SerializedName("download_location") val downloadEndpoint: String? = null,
+  @SerializedName("download_location") val downloadEndpoint: String,
 )
