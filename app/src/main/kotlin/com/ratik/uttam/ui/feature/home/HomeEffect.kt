63d31d028644d68bffc21fe31235f7811d89c98b
Ratik Sharma <ratik96@gmail.com>
2024-06-10 01:38:22 +0530

    feat(set-automatically): ensure automatic wallpaper set on manual refresh (#77)

--- app/src/main/kotlin/com/ratik/uttam/ui/feature/home/HomeEffect.kt
+++ app/src/main/kotlin/com/ratik/uttam/ui/feature/home/HomeEffect.kt
@@ -3,5 +3,7 @@ package com.ratik.uttam.ui.feature.home
 import com.fueled.android.core.common.contract.SideEffect
 
 internal sealed class HomeEffect : SideEffect {
-  data object ChangeWallpaper : HomeEffect()
+  data object LaunchCropAndSetWallpaperFlow : HomeEffect()
+
+  data object SetWallpaperSilently : HomeEffect()
 }
