251b44bed3c9e89af65dc68dd8396040a251700b
Ratik Sharma <ratik96@gmail.com>
2024-06-10 02:19:53 +0530

    chore(google-services): remove google services plugin

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,7 +4,6 @@ import java.util.Properties
 plugins {
   alias(libs.plugins.android.application)
   alias(libs.plugins.kotlin.android)
-  alias(libs.plugins.google.services)
   kotlin("kapt")
   alias(libs.plugins.hilt)
   alias(libs.plugins.ksp)
