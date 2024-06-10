5a4480619e666df96110d83cbdd7d409bd929ad6
Ratik Sharma <ratik96@gmail.com>
2024-06-10 17:12:26 +0530

    feat(collections): add more wallpaper collections

--- app/src/main/kotlin/com/ratik/uttam/data/ApiConstants.kt
+++ app/src/main/kotlin/com/ratik/uttam/data/ApiConstants.kt
@@ -2,6 +2,6 @@ package com.ratik.uttam.data
 
 object ApiConstants {
   const val BASE_URL = "https://api.unsplash.com/"
-  const val DEFAULT_COLLECTIONS = "935527,881815,791207"
+  const val DEFAULT_COLLECTIONS = "935527,881815,791207,1065976,914227,460827"
   const val DEFAULT_WALLPAPER_ID = "86UVUXh3djE" // TODO: Change this to a wallpaper that you own
 }
